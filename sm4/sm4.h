0f42534a1c2457b13bbdbf867a67c6ca11fd29d2
hustcw <hustcw1998@gmail.com>
2020-11-09 23:51:45 +0800

    sm4 finish sync

---
+++ sm4/sm4.h
@@ -0,0 +1,23 @@
+#include <stdint.h>
+
+#define SM4KeyLen 16
+#define KeyExpSize 32
+#define IvSize 4
+
+
+typedef struct SM4_context {
+    uint32_t RoundKey[KeyExpSize];
+    uint32_t Iv[IvSize]; // using cbc mode
+} SM4_Context;
+
+// initiallize the sm4_ctx
+void SM4_Init(SM4_Context* sm4_ctx, uint8_t* key, uint8_t* iv);
+
+// cbc encryption
+void SM4_Encrypt(SM4_Context* sm4_ctx, uint8_t* msg, int length);
+
+// cbc decryption
+void SM4_Decrypt(SM4_Context* sm4_ctx, uint8_t* cipher, int length);
+
+void KeyExpansion(uint32_t* RoundKey, uint32_t* Key);
+
